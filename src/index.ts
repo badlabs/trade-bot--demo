f5ac69ef39773709b6224f7717d8988ecd24ea1e
Nikolay Dorofeev <dorich2000@gmail.com>
2022-11-20 11:11:50 +0400

    Run with function

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import {TradeBot} from "trade-bot-core";
+import {runTradeBot} from "trade-bot-core";
 import {ExchangeClient} from "./exchange-client";
 import {initAlgorithms} from "./algorithms";
 
-const tradebot = new TradeBot({
+runTradeBot({
   exchangeClient: new ExchangeClient(process.env.TINKOFF_SANDBOX_API_KEY || ''),
   botToken: process.env.BOT_TOKEN || '',
   initAlgorithmsCallback: initAlgorithms
